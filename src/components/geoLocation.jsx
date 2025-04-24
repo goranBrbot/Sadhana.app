118347b300f027e2a4617aba3b9252d26132815b
Goran Brbot <goran.brbot@mail.com>
2025-04-24 15:37:10 +0200

    fix

--- src/components/geoLocation.jsx
+++ src/components/geoLocation.jsx
@@ -51,7 +51,7 @@ export default function GeoFindMe({ setLocation }) {
   async function fetchAltitude(latitude, longitude, gpsAltitude) {
     // Ako GPS ne vraća visinu, koristi Open-Meteo API
     if (gpsAltitude !== null) {
-      return gpsAltitude;
+      return gpsAltitude.toPrecision(3); // number to 3 numbers
     }
     try {
       const response = await fetch(`https://api.open-meteo.com/v1/elevation?latitude=${latitude}&longitude=${longitude}`);
